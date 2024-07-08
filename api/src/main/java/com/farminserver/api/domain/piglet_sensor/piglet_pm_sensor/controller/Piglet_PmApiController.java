de776c430cd7c311d84e082bbc7ef7c99a7b6617
cheonsebin <lemon2842@naver.com>
2024-07-08 17:34:28 +0900

    piglet_PmApiController 내용 수정

--- api/src/main/java/com/farminserver/api/domain/piglet_sensor/piglet_pm_sensor/controller/Piglet_PmApiController.java
+++ api/src/main/java/com/farminserver/api/domain/piglet_sensor/piglet_pm_sensor/controller/Piglet_PmApiController.java
@@ -1,7 +1,8 @@
 package com.farminserver.api.domain.piglet_sensor.piglet_pm_sensor.controller;
 
-import com.farminserver.api.domain.boars_sensor.boars_co2_sensor.business.UserBusiness;
-import com.farminserver.api.domain.boars_sensor.boars_co2_sensor.controller.model.UserResponse;
+
+import com.farminserver.api.domain.piglet_sensor.piglet_pm_sensor.business.Piglet_PmBusiness;
+import com.farminserver.api.domain.piglet_sensor.piglet_pm_sensor.controller.model.Piglet_PmResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
@@ -9,36 +10,37 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/api/boars_co2")
+@RequestMapping("/api/piglet_co2")
 public class Piglet_PmApiController {
 
-    private final UserBusiness userBusiness;
+    private final Piglet_PmBusiness pigletPmBusiness;
 
     @Autowired
-    public Piglet_PmApiController(UserBusiness userBusiness) {
-        this.userBusiness = userBusiness;
+    public Piglet_PmApiController(Piglet_PmBusiness pigletPmBusiness) {
+        this.pigletPmBusiness = pigletPmBusiness;
     }
 
-    @GetMapping("/co2data")
-    public UserResponse getCo2SensorData() {
-        return userBusiness.getCo2SensorData();
+    @GetMapping("/pmdata/{pigletRoomNum}")
+    public ResponseEntity<Piglet_PmResponse> getPmSensorData(@PathVariable String pigletRoomNum) {
+        return ResponseEntity.ok(pigletPmBusiness.getPmSensorData(pigletRoomNum));
     }
 
     @GetMapping("/export")
-    public ResponseEntity<Resource> exportCo2Data() {
-        String filePath = "co2_data.xlsx";
+    public ResponseEntity<Resource> exportPmData() {
+        String filePath = "pm_data.xlsx";
         try {
-            userBusiness.exportCo2DataToExcel(filePath);
+            pigletPmBusiness.exportPmDataToExcel(filePath);
             Resource resource = new FileSystemResource(filePath);
 
-            HttpHeaders headers = new HttpHeaders();
-            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=co2_data.xlsx");
+            org.springframework.http.HttpHeaders headers = new org.springframework.http.HttpHeaders();
+            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=pm_data.xlsx");
 
             return new ResponseEntity<>(resource, headers, HttpStatus.OK);
         } catch (IOException e) {
