9142ec59bce1cdfa086b1d82aa110b610d096dc4
cheonsebin <lemon2842@naver.com>
2024-07-08 15:01:21 +0900

    piglet_ExcelExporter 생성

---
+++ api/src/main/java/com/farminserver/api/util/Piglet_ExcelExporter.java
@@ -0,0 +1,153 @@
+package com.farminserver.api.util;
+
+import com.farminserver.api.domain.boars_sensor.boars_co2_sensor.controller.model.Boar_Co2Response;
+import com.farminserver.api.domain.boars_sensor.boars_humidity_sensor.controller.model.Boars_HumidityResponse;
+import com.farminserver.api.domain.boars_sensor.boars_nh3_sensor.controller.model.Boars_Nh3Response;
+import com.farminserver.api.domain.boars_sensor.boars_pm_sensor.controller.model.Boars_PmResponse;
+import com.farminserver.api.domain.boars_sensor.boars_temperature_sensor.controller.model.Boars_TemperatureResponse;
+import com.farminserver.api.domain.piglet_sensor.piglet_co2_sensor.controller.model.Piglet_Co2Response;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.stereotype.Component;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+@Component
+
+public class Piglet_ExcelExporter {
+
+    public void exportBoars_Nh3Data(List<Boars_Nh3Response> responses, String filePath) throws IOException {
+        Workbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet("Nh3 Data");
+
+        int rowNum = 0;
+        Row headerRow = sheet.createRow(rowNum++);
+        headerRow.createCell(0).setCellValue("Room Number");
+        headerRow.createCell(1).setCellValue("Nh3");
+        headerRow.createCell(2).setCellValue("Unit");
+        headerRow.createCell(3).setCellValue("Timestamp");
+
+        for (Boars_Nh3Response response : responses) {
+            Row row = sheet.createRow(rowNum++);
+            row.createCell(0).setCellValue(response.getBoarsBarnRoomNum());
+            row.createCell(1).setCellValue(response.getBoarsNh3Data());
+            row.createCell(2).setCellValue(response.getUnit());
+            row.createCell(3).setCellValue(response.getTimestamp());
+        }
+
+        try (FileOutputStream fileOut = new FileOutputStream(filePath)) {
+            workbook.write(fileOut);
+        }
+        workbook.close();
+    }
+
+    public void exportPiglet_co2Data(List<Piglet_Co2Response> responses, String filePath) throws IOException {
+        Workbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet("Co2 Data");
+
+        int rowNum = 0;
+        Row headerRow = sheet.createRow(rowNum++);
+        headerRow.createCell(0).setCellValue("Room Number");
+        headerRow.createCell(1).setCellValue("Co2 Data");
+        headerRow.createCell(2).setCellValue("Unit");
+        headerRow.createCell(3).setCellValue("Timestamp");
+
+        for (Piglet_Co2Response response : responses) {
+            Row row = sheet.createRow(rowNum++);
+            row.createCell(0).setCellValue(response.getPigletRoomNum());
+            row.createCell(1).setCellValue(response.getPigletCo2Data());
+            row.createCell(2).setCellValue(response.getUnit());
+            row.createCell(3).setCellValue(response.getTimestamp());
+        }
+
+        try (FileOutputStream fileOut = new FileOutputStream(filePath)) {
+            workbook.write(fileOut);
+        }
+        workbook.close();
+    }
+
+    public void exportBoars_humidityData(List<Boars_HumidityResponse> responses, String filePath) throws IOException {
+        Workbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet("Humidity Data");
+
+        int rowNum = 0;
+        Row headerRow = sheet.createRow(rowNum++);
+        headerRow.createCell(0).setCellValue("Room Number");
+        headerRow.createCell(1).setCellValue("Humidity Data");
+        headerRow.createCell(2).setCellValue("Unit");
+        headerRow.createCell(3).setCellValue("Timestamp");
+
+        for (Boars_HumidityResponse response : responses) {
+            Row row = sheet.createRow(rowNum++);
+            row.createCell(0).setCellValue(response.getBoarsBarnRoomNum());
+            row.createCell(1).setCellValue(response.getBoarsHumidityData());
+            row.createCell(2).setCellValue(response.getUnit());
+            row.createCell(3).setCellValue(response.getTimestamp());
+        }
+
+        try (FileOutputStream fileOut = new FileOutputStream(filePath)) {
+            workbook.write(fileOut);
+        }
+        workbook.close();
+    }
+
+    public void exportBoars_TemperatureData(List<Boars_TemperatureResponse> boarsTemperatureRespons, String filePath) throws IOException {
+        Workbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet("Temperature Data");
+
+        int rowNum = 0;
+        Row headerRow = sheet.createRow(rowNum++);
+        headerRow.createCell(0).setCellValue("Room Number");
+        headerRow.createCell(1).setCellValue("Temperature Data");
+        headerRow.createCell(2).setCellValue("Unit");
+        headerRow.createCell(3).setCellValue("Timestamp");
+        headerRow.createCell(4).setCellValue("Temperature locate Data");
+
+        for (Boars_TemperatureResponse response : boarsTemperatureRespons) {
+            Row row = sheet.createRow(rowNum++);
+            row.createCell(0).setCellValue(response.getBoarsBarnRoomNum());
+            row.createCell(1).setCellValue(response.getBoarsTemperatureData());
+            row.createCell(2).setCellValue(response.getUnit());
+            row.createCell(3).setCellValue(response.getTimestamp());
+            row.createCell(4).setCellValue(response.getboarsTemperatureLocateData());
+        }
+
+        try (FileOutputStream fileOut = new FileOutputStream(filePath)) {
+            workbook.write(fileOut);
+        }
+        workbook.close();
+    }
+
+    public void exportBoars_PmData(List<Boars_PmResponse> boarsPmRespons, String filePath) throws IOException {
+        Workbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet("PM Data");
+
+        int rowNum = 0;
+        Row headerRow = sheet.createRow(rowNum++);
+        headerRow.createCell(0).setCellValue("PM1.0");
+        headerRow.createCell(1).setCellValue("PM2.5");
+        headerRow.createCell(2).setCellValue("PM10");
+        headerRow.createCell(3).setCellValue("Total PM");
+        headerRow.createCell(4).setCellValue("Timestamp");
+
+        for (Boars_PmResponse response : boarsPmRespons) {
+            Row row = sheet.createRow(rowNum++);
+            row.createCell(0).setCellValue(response.getPm1_0());
+            row.createCell(1).setCellValue(response.getPm2_5());
+            row.createCell(2).setCellValue(response.getPm10());
+            row.createCell(3).setCellValue(response.getTotalPm());
+            row.createCell(4).setCellValue(response.getTimestamp());
+        }
+
+        try (FileOutputStream fileOut = new FileOutputStream(filePath)) {
+            workbook.write(fileOut);
+        }
+        workbook.close();
+
+
+    }
+}
