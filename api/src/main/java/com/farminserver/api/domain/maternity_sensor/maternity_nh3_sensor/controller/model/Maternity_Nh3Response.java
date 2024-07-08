ee4f704efee73ce6b4aba1edc7657752c94bb598
cheonsebin <lemon2842@naver.com>
2024-07-08 09:10:52 +0900

    maternity_nh3의 각 class 이름 변경

---
+++ api/src/main/java/com/farminserver/api/domain/maternity_sensor/maternity_nh3_sensor/controller/model/Maternity_Nh3Response.java
@@ -0,0 +1,44 @@
+package com.farminserver.api.domain.boars_sensor.boars_co2_sensor.controller.model;
+
+public class Maternity_Nh3Response {
+    private String boarsBarnRoomNum;
+    private double BoarsNh3;
+    private String unit;
+    private long timestamp;
+
+    public Maternity_Nh3Response(double co2, String unit, long timestamp) {
+        this.boarsBarnRoomNum = boarsBarnRoomNum;
+        this.BoarsNh3 = boarsnh3;
+        this.unit = unit;
+        this.timestamp = timestamp;
+    }
+
+    // Getters and Setters
+    public String getBoarsBarnRoomNum() {
+        return boarsBarnRoomNum;
+    }
+    public void setBoarsBarnRoomNum(String boarsBarnRoomNum) {
+        this.boarsBarnRoomNum = boarsBarnRoomNum;
+    }
+
+    public double getBoarsNh3Data() {
+        return BoarsNh3;
+    }
+    public void setBoarsNh3Data(double boarsNh3) {
+        this.BoarsNh3 = boarsNh3 ;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
