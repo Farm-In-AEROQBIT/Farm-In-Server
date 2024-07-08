61424e7a297cb9dd8885c7f5be04633fdd225728
cheonsebin <lemon2842@naver.com>
2024-07-08 16:50:40 +0900

    pigletNh3 패키지의 각 class 이름과 package 경로 수정

--- api/src/main/java/com/farminserver/api/domain/piglet_sensor/piglet_nh3_sensor/controller/Piglet_Nh3ApiController.java
+++ api/src/main/java/com/farminserver/api/domain/piglet_sensor/piglet_nh3_sensor/controller/Piglet_Nh3ApiController.java
@@ -1,7 +1,9 @@
-package com.farminserver.api.domain.boars_sensor.boars_co2_sensor.controller;
+package com.farminserver.api.domain.piglet_sensor.piglet_nh3_sensor.controller;
 
 import com.farminserver.api.domain.boars_sensor.boars_co2_sensor.business.UserBusiness;
 import com.farminserver.api.domain.boars_sensor.boars_co2_sensor.controller.model.UserResponse;
+import com.farminserver.api.domain.boars_sensor.boars_nh3_sensor.business.Boars_Nh3Business;
+import com.farminserver.api.domain.boars_sensor.boars_nh3_sensor.controller.model.Boars_Nh3Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
@@ -9,6 +11,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,29 +19,29 @@ import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/boars_co2")
-public class Co2ApiController {
+public class Piglet_Nh3ApiController {
 
-    private final UserBusiness userBusiness;
+    private final Boars_Nh3Business boarsNh3Business;
 
     @Autowired
-    public Co2ApiController(UserBusiness userBusiness) {
-        this.userBusiness = userBusiness;
+    public Boars_Nh3ApiController(Boars_Nh3Business Nh3Business) {
+        this.boarsNh3Business = Nh3Business;
     }
 
-    @GetMapping("/co2data")
-    public UserResponse getCo2SensorData() {
-        return userBusiness.getCo2SensorData();
+    @GetMapping("/nh3data/{boarsBarnRoomNum}")
+    public ResponseEntity<Boars_Nh3Response> getNh3SensorData(@PathVariable String boarsBarnRoomNum) {
+        return ResponseEntity.ok(boarsNh3Business.getNh3SensorData(boarsBarnRoomNum));
     }
 
     @GetMapping("/export")
-    public ResponseEntity<Resource> exportCo2Data() {
-        String filePath = "co2_data.xlsx";
+    public ResponseEntity<Resource> exportNh3Data() {
+        String filePath = "baors_nh3_data.xlsx";
         try {
-            userBusiness.exportCo2DataToExcel(filePath);
+            boarsNh3Business.exportBoars_Nh3DataToExcel(filePath);
             Resource resource = new FileSystemResource(filePath);
 
             HttpHeaders headers = new HttpHeaders();
-            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=co2_data.xlsx");
+            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=nh3_data.xlsx");
 
             return new ResponseEntity<>(resource, headers, HttpStatus.OK);
         } catch (IOException e) {
---
+++ api/src/main/java/com/farminserver/api/domain/piglet_sensor/piglet_nh3_sensor/controller/model/Piglet_Nh3Response.java
@@ -0,0 +1,45 @@
+package com.farminserver.api.domain.piglet_sensor.piglet_nh3_sensor.controller.model;
+
+public class Piglet_Nh3Response {
+    private String boarsBarnRoomNum;
+    private double BoarsNh3;
+    private String unit;
+    private long timestamp;
+
+    public Boars_Nh3Response(String boarsBarnRoomNum, double boarsnh3, String unit, long timestamp) {
+        this.boarsBarnRoomNum = boarsBarnRoomNum;
+        this.BoarsNh3 = boarsnh3;
+        this.unit = unit;
+        this.timestamp = timestamp;
+    }
+
+    // Getters and Setters
+
+    public String getBoarsBarnRoomNum() {
+        return boarsBarnRoomNum;
+    }
+    public void setBoarsBarnRoomNum(String boarsBarnRoomNum) {
+        this.boarsBarnRoomNum = boarsBarnRoomNum;
+    }
+
+    public double getBoarsNh3Data() {
+        return BoarsNh3;
+    }
+    public void setBoarsNh3Data(double boarsNh3) {
+        this.BoarsNh3 = boarsNh3 ;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
