206b470f2affc05f0a37fb060db9dd89d90a732c
cheonsebin <lemon2842@naver.com>
2024-07-08 15:34:06 +0900

    piglet HumidityConverter 클래스 내용 수정

--- api/src/main/java/com/farminserver/api/domain/piglet_sensor/piglet_humidity_sensor/converter/Piglet_HumidityConverter.java
+++ api/src/main/java/com/farminserver/api/domain/piglet_sensor/piglet_humidity_sensor/converter/Piglet_HumidityConverter.java
@@ -1,10 +1,8 @@
 package com.farminserver.api.domain.piglet_sensor.piglet_humidity_sensor.converter;
 
-import com.farminserver.api.domain.boars_sensor.boars_co2_sensor.controller.model.UserResponse;
-import com.farminserver.api.domain.boars_sensor.boars_humidity_sensor.controller.model.Boars_HumidityResponse;
-import com.farminserver.db.boars_humidity_sensor.Boars_HumiditySensorEntity;
+import com.farminserver.api.domain.piglet_sensor.piglet_humidity_sensor.controller.model.Piglet_HumidityResponse;
+import com.farminserver.db.piglet_humidity_sensor.Piglet_HumiditySensorEntity;
 import org.springframework.stereotype.Component;
-import com.farminserver.db.boars_co2_sensor.CO2SensorEntity;
 import com.farminserver.api.common.exception.ApiException;
 import com.farminserver.api.common.error.ErrorCode;
 
@@ -14,13 +12,13 @@ import java.util.Optional;
 @Component
 public class Piglet_HumidityConverter {
 
-    public Boars_HumidityResponse convert(Boars_HumiditySensorEntity humiditySensorEntity) {
+    public Piglet_HumidityResponse convert(Piglet_HumiditySensorEntity humiditySensorEntity) {
         return Optional.ofNullable(humiditySensorEntity)
-                .map(entity -> new Boars_HumidityResponse(
-                        entity.getBoars_Barn_Room_Num(),
-                        entity.getBoars_Humidity_Data(),
+                .map(entity -> new Piglet_HumidityResponse(
+                        entity.getFarmName(),
+                        entity.getPiglet_Humidity_Data(),
                         "%",
-                        entity.getboars_Humidity_Input_Time().toEpochSecond(ZoneOffset.UTC)
+                        entity.getpiglet_Humidity_Input_Time().toEpochSecond(ZoneOffset.UTC)
                 ))
                 .orElseThrow(() -> new ApiException(ErrorCode.NULL_POINT));
     }
