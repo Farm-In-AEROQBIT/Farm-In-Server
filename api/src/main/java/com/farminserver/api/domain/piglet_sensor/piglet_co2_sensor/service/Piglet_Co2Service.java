ef4d23029ee49615d3c70bff12a2519e82ac2ece
cheonsebin <lemon2842@naver.com>
2024-07-08 14:58:52 +0900

    piglet_Co2Servie 내용 수정

--- api/src/main/java/com/farminserver/api/domain/piglet_sensor/piglet_co2_sensor/service/Piglet_Co2Service.java
+++ api/src/main/java/com/farminserver/api/domain/piglet_sensor/piglet_co2_sensor/service/Piglet_Co2Service.java
@@ -1,15 +1,13 @@
 package com.farminserver.api.domain.piglet_sensor.piglet_co2_sensor.service;
 
-import com.farminserver.api.domain.boars_sensor.boars_co2_sensor.controller.model.Boar_Co2Response;
-import com.farminserver.api.domain.boars_sensor.boars_co2_sensor.controller.model.UserResponse;
-import com.farminserver.api.domain.boars_sensor.boars_co2_sensor.converter.Boars_Co2Converter;
-import com.farminserver.api.util.Boars_ExcelExporter;
-import com.farminserver.db.boars_co2_sensor.Boars_Co2SensorEntity;
-import com.farminserver.db.boars_co2_sensor.Boars_Co2SensorRepository;
+import com.farminserver.api.domain.piglet_sensor.piglet_co2_sensor.controller.model.Piglet_Co2Response;
+import com.farminserver.api.domain.piglet_sensor.piglet_co2_sensor.converter.Piglet_Co2Converter;
+
+import com.farminserver.db.piglet_co2_sensor.Piglet_Co2SensorEntity;
+import com.farminserver.db.piglet_co2_sensor.Piglet_Co2SensorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.farminserver.db.boars_co2_sensor.CO2SensorRepository;
-import com.farminserver.db.boars_co2_sensor.CO2SensorEntity;
+
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -18,33 +16,33 @@ import java.util.List;
 @Service
 public class Piglet_Co2Service {
 
-    private final Boars_Co2SensorRepository repository;
-    private final Boars_Co2Converter converter;
-    private final Boars_ExcelExporter excelExporter;
+    private final Piglet_Co2SensorRepository repository;
+    private final Piglet_Co2Converter converter;
+    private final Piglet_ExcelExporter excelExporter;
 
     @Autowired
-    public Boars_Co2Service(Boars_Co2SensorRepository repository, Boars_Co2Converter converter, Boars_ExcelExporter excelExporter) {
+    public Piglet_Co2Service(Piglet_Co2SensorRepository repository, Piglet_Co2Converter converter, Piglet_ExcelExporter excelExporter) {
         this.repository = repository;
         this.converter = converter;
         this.excelExporter = excelExporter;
     }
 
-    public Boar_Co2Response getCo2Data(String boarsBarnRoomNum) {
-        Boars_Co2SensorEntity entity = repository.findById(boarsBarnRoomNum).orElseThrow(() -> new RuntimeException("Sensor data not found"));
+    public Piglet_Co2Response getCo2Data(String pigletRoomNum) {
+        Piglet_Co2SensorEntity entity = repository.findById(pigletRoomNum).orElseThrow(() -> new RuntimeException("Sensor data not found"));
         return converter.convert(entity);
     }
 
-    public List<Boar_Co2Response> getAllCo2Data() {
-        List<Boars_Co2SensorEntity> entities = repository.findAll();
-        List<Boar_Co2Response> responses = new ArrayList<>();
-        for (Boars_Co2SensorEntity entity : entities) {
+    public List<Piglet_Co2Response> getAllCo2Data() {
+        List<Piglet_Co2SensorEntity> entities = repository.findAll();
+        List<Piglet_Co2Response> responses = new ArrayList<>();
+        for (Piglet_Co2SensorEntity entity : entities) {
             responses.add(converter.convert(entity));
         }
         return responses;
     }
 
     public void exportCo2DataToExcel(String filePath) throws IOException {
-        List<Boar_Co2Response> responses = getAllCo2Data();
-        excelExporter.exportBoars_co2Data(responses, filePath);
+        List<Piglet_Co2Response> responses = getAllCo2Data();
+        excelExporter.exportPiglet_co2Data(responses, filePath);
     }
 }
