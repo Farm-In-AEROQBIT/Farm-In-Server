5a46e24f7671a3b45e7f32580189b42c21fb5863
cheonsebin <lemon2842@naver.com>
2024-07-08 17:39:12 +0900

    piglet_PmApiConverter 내용 수정

--- api/src/main/java/com/farminserver/api/domain/piglet_sensor/piglet_pm_sensor/converter/Piglet_PmConverter.java
+++ api/src/main/java/com/farminserver/api/domain/piglet_sensor/piglet_pm_sensor/converter/Piglet_PmConverter.java
@@ -1,28 +1,27 @@
 package com.farminserver.api.domain.piglet_sensor.piglet_pm_sensor.converter;
 
-import com.farminserver.api.domain.boars_sensor.boars_co2_sensor.controller.model.UserResponse;
+import com.farminserver.api.domain.piglet_sensor.piglet_pm_sensor.controller.model.Piglet_PmResponse;
+import com.farminserver.db.piglet_pm_sensor.Piglet_PmSensorEntity;
 import org.springframework.stereotype.Component;
-import com.farminserver.db.boars_co2_sensor.CO2SensorEntity;
 import com.farminserver.api.common.exception.ApiException;
 import com.farminserver.api.common.error.ErrorCode;
 
+import java.time.ZoneOffset;
 import java.util.Optional;
 
 @Component
 public class Piglet_PmConverter {
 
-    public UserResponse convert(double co2) {
-        String unit = "ppm";
-        long timestamp = System.currentTimeMillis();
-        return new UserResponse(co2, unit, timestamp);
-    }
-
-    public UserResponse toResponse(CO2SensorEntity co2SensorEntity) {
-        return Optional.ofNullable(co2SensorEntity)
-                .map(entity -> new UserResponse(
-                        entity.getCo2Data(),
+    public Piglet_PmResponse convert(Piglet_PmSensorEntity pmSensorEntity) {
+        return Optional.ofNullable(pmSensorEntity)
+                .map(entity -> new Piglet_PmResponse(
+                        entity.getPiglet_Barn_Room_Num(),
+                        entity.getPiglet_Pm1_Data(),
+                        entity.getPiglet_Pm25_Data(),
+                        entity.getPiglet_Pm10_Data(),
+                        entity.getPiglet_Total_Pm_Data(),
                         "ppm",
-                        entity.getCo2InputTime().atZone(java.time.ZoneId.systemDefault()).toInstant().toEpochMilli()
+                        entity.getpiglet_Pm_Input_Time().toEpochSecond(ZoneOffset.UTC)
                 ))
                 .orElseThrow(() -> new ApiException(ErrorCode.NULL_POINT));
     }
