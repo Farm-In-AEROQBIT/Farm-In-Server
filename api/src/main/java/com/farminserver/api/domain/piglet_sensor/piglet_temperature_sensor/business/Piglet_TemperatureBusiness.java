7a94c9b32da6882425ef47da938c7d884a4b98fb
cheonsebin <lemon2842@naver.com>
2024-07-08 17:53:54 +0900

    piglet_temperatureBusiness의 내용 수정

--- api/src/main/java/com/farminserver/api/domain/piglet_sensor/piglet_temperature_sensor/business/Piglet_TemperatureBusiness.java
+++ api/src/main/java/com/farminserver/api/domain/piglet_sensor/piglet_temperature_sensor/business/Piglet_TemperatureBusiness.java
@@ -1,7 +1,7 @@
 package com.farminserver.api.domain.piglet_sensor.piglet_temperature_sensor.business;
 
-import com.farminserver.api.domain.boars_sensor.boars_co2_sensor.service.UserService;
-import com.farminserver.api.domain.boars_sensor.boars_co2_sensor.controller.model.UserResponse;
+import com.farminserver.api.domain.piglet_sensor.piglet_temperature_sensor.controller.model.Piglet_TemperatureResponse;
+import com.farminserver.api.domain.piglet_sensor.piglet_temperature_sensor.service.Piglet_TemperatureService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -11,23 +11,22 @@ import java.util.List;
 @Component
 public class Piglet_TemperatureBusiness {
 
-    private final UserService userService;
+    private final Piglet_TemperatureService pigletTemperatureService;
 
     @Autowired
-    public Piglet_TemperatureBusiness(UserService userService) {
-        this.userService = userService;
+    public Piglet_TemperatureBusiness(Piglet_TemperatureService pigletTemperatureService) {
+        this.pigletTemperatureService = pigletTemperatureService;
     }
 
-    public UserResponse getCo2SensorData() {
-        double co2Data = userService.getCo2Data();
-        return new UserResponse(co2Data, "ppm", System.currentTimeMillis());
+    public Piglet_TemperatureResponse getTemperatureSensorData(String pigletRoomNum) {
+        return pigletTemperatureService.getTemperatureData(pigletRoomNum);
     }
 
-    public List<UserResponse> getAllCo2Data() {
-        return userService.getAllCo2Data();
+    public List<Piglet_TemperatureResponse> getAllTemperatureData() {
+        return pigletTemperatureService.getAllTemperatureData();
     }
 
-    public void exportCo2DataToExcel(String filePath) throws IOException {
-        userService.exportCo2DataToExcel(filePath);
+    public void exportTemperatureDataToExcel(String filePath) throws IOException {
+        pigletTemperatureService.exportTemperatureDataToExcel(filePath);
     }
 }
