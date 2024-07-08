bb876c1be3d9ee2ccc842115ee801f10c5c7e9c9
cheonsebin <lemon2842@naver.com>
2024-07-08 13:01:09 +0900

    maternity_PmResponse 수정

--- api/src/main/java/com/farminserver/api/domain/maternity_sensor/maternity_pm_sensor/controller/model/Maternity_PmResponse.java
+++ api/src/main/java/com/farminserver/api/domain/maternity_sensor/maternity_pm_sensor/controller/model/Maternity_PmResponse.java
@@ -1,23 +1,62 @@
 package com.farminserver.api.domain.maternity_sensor.maternity_pm_sensor.controller.model;
 
 public class Maternity_PmResponse {
-    private double co2;
+    private String MaternityRoomNum;
+    private double pm1_0;
+    private double pm2_5;
+    private double pm10;
+    private double totalPm;
     private String unit;
     private long timestamp;
 
-    public Maternity_PmResponse(double co2, String unit, long timestamp) {
-        this.co2 = co2;
+    public Maternity_PmResponse(String MaternityRoomNum, double pm1_0, double pm2_5, double pm10, double totalPm, String unit, long timestamp) {
+        this.MaternityRoomNum = MaternityRoomNum;
+        this.pm1_0 = pm1_0;
+        this.pm2_5 = pm2_5;
+        this.pm10 = pm10;
+        this.totalPm = totalPm;
         this.unit = unit;
         this.timestamp = timestamp;
     }
 
-    // Getters and Setters
-    public double getCo2() {
-        return co2;
+
+    public String getMaternityRoomNum() {
+        return MaternityRoomNum;
+    }
+    public void setMaternityRoomNum(String MaternityRoomNum) {
+        this.MaternityRoomNum = MaternityRoomNum;
     }
 
-    public void setSensorValue(double co2) {
-        this.co2 = co2;
+    public double getPm1_0() {
+        return pm1_0;
+    }
+
+    public void setPm1_0(double pm1_0) {
+        this.pm1_0 = pm1_0;
+    }
+
+    public double getPm2_5() {
+        return pm2_5;
+    }
+
+    public void setPm2_5(double pm2_5) {
+        this.pm2_5 = pm2_5;
+    }
+
+    public double getPm10() {
+        return pm10;
+    }
+
+    public void setPm10(double pm10) {
+        this.pm10 = pm10;
+    }
+
+    public double getTotalPm() {
+        return totalPm;
+    }
+
+    public void setTotalPm(double totalPm) {
+        this.totalPm = totalPm;
     }
 
     public String getUnit() {
@@ -35,4 +74,5 @@ public class Maternity_PmResponse {
     public void setTimestamp(long timestamp) {
         this.timestamp = timestamp;
     }
+
 }
