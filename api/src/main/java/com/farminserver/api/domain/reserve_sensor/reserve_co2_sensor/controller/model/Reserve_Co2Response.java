410f91388bd8c83cd9b2ba43c35812119ead7fe2
cheonsebin <lemon2842@naver.com>
2024-07-09 13:47:15 +0900

    Reserve Co2 Response 내용 수정

--- api/src/main/java/com/farminserver/api/domain/reserve_sensor/reserve_co2_sensor/controller/model/Reserve_Co2Response.java
+++ api/src/main/java/com/farminserver/api/domain/reserve_sensor/reserve_co2_sensor/controller/model/Reserve_Co2Response.java
@@ -1,29 +1,37 @@
 package com.farminserver.api.domain.reserve_sensor.reserve_co2_sensor.controller.model;
 
+
 public class Reserve_Co2Response {
-    private double co2;
+    private String reserveSowsRoomNum;
+    private double ReserveCo2Data;
     private String unit;
     private long timestamp;
 
-    public Reserve_Co2Response(double co2, String unit, long timestamp) {
-        this.co2 = co2;
+    public Reserve_Co2Response(String reserveSowsRoomNum, double reserveCo2, String unit, long timestamp) {
+        this.reserveSowsRoomNum = reserveSowsRoomNum;
+        this.ReserveCo2Data = reserveCo2;
         this.unit = unit;
         this.timestamp = timestamp;
     }
 
     // Getters and Setters
-    public double getCo2() {
-        return co2;
+    public String getReserveSowRoomNum() {
+        return reserveSowsRoomNum;
+    }
+    public void setReserveSowsRoomNum(String reserveSowsRoomNum) {
+        this.reserveSowsRoomNum = reserveSowsRoomNum;
     }
 
-    public void setSensorValue(double co2) {
-        this.co2 = co2;
+    public double getReserveCo2Data() {
+        return ReserveCo2Data;
+    }
+    public void setReserveCo2(double reserveCo2) {
+        this.ReserveCo2Data = reserveCo2;
     }
 
     public String getUnit() {
         return unit;
     }
-
     public void setUnit(String unit) {
         this.unit = unit;
     }
@@ -31,7 +39,6 @@ public class Reserve_Co2Response {
     public long getTimestamp() {
         return timestamp;
     }
-
     public void setTimestamp(long timestamp) {
         this.timestamp = timestamp;
     }
