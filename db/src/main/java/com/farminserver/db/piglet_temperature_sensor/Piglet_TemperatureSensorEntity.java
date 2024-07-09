27987e80e8376f549763a85d64e54ab849ef8654
cheonsebin <lemon2842@naver.com>
2024-07-09 09:12:25 +0900

    piglet_TemperatureSensorEntity의 TemperInput_Time과 Tmper_Locate_Data의 자료형 변경

--- db/src/main/java/com/farminserver/db/piglet_temperature_sensor/Piglet_TemperatureSensorEntity.java
+++ db/src/main/java/com/farminserver/db/piglet_temperature_sensor/Piglet_TemperatureSensorEntity.java
@@ -8,6 +8,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.persistence.Id;
 
+import java.time.LocalDateTime;
+
 
 @EqualsAndHashCode(callSuper = false)
 @Data
@@ -23,8 +25,8 @@ public class Piglet_TemperatureSensorEntity extends BaseEntity {
     @Id
     private String Piglet_Room_Num;
     private double Piglet_Temper_Data;
-    private String Piglet_Temper_Input_Time;
-    private String Piglet_Temper_Locate_Data;
+    private LocalDateTime Piglet_Temper_Input_Time;
+    private long Piglet_Temper_Locate_Data;
 
     // Getters and Setters
     public String getPigletRoomNum() {
@@ -45,19 +47,19 @@ public class Piglet_TemperatureSensorEntity extends BaseEntity {
     }
 
 
-    public String getPigletTemperInputTime() {
+    public LocalDateTime getPigletTemperInputTime() {
         return Piglet_Temper_Input_Time;
     }
 
-    public void setPigletTemperInputTime(String Piglet_Temper_Input_Time) {
+    public void setPigletTemperInputTime(LocalDateTime Piglet_Temper_Input_Time) {
         this.Piglet_Temper_Input_Time = Piglet_Temper_Input_Time;
     }
 
-    public String getPigletTemperLocateData() {
+    public long getPigletTemperLocateData() {
         return Piglet_Temper_Locate_Data;
     }
 
-    public void setPigletTemperLocateData(String Piglet_Temper_Locate_Data) {
+    public void setPigletTemperLocateData(long Piglet_Temper_Locate_Data) {
         this.Piglet_Temper_Locate_Data = Piglet_Temper_Locate_Data;
     }
 
