e79717162c745c101777da6709f31da2ed81c824
cheonsebin <lemon2842@naver.com>
2024-07-09 13:49:36 +0900

    Reserve_Co2 ApiController 내용 수정

--- api/src/main/java/com/farminserver/api/domain/reserve_sensor/reserve_co2_sensor/controller/Reserve_Co2ApiController.java
+++ api/src/main/java/com/farminserver/api/domain/reserve_sensor/reserve_co2_sensor/controller/Reserve_Co2ApiController.java
@@ -2,6 +2,8 @@ package com.farminserver.api.domain.reserve_sensor.reserve_co2_sensor.controller
 
 import com.farminserver.api.domain.boars_sensor.boars_co2_sensor.business.UserBusiness;
 import com.farminserver.api.domain.boars_sensor.boars_co2_sensor.controller.model.UserResponse;
+import com.farminserver.api.domain.reserve_sensor.reserve_co2_sensor.business.Reserve_Co2Business;
+import com.farminserver.api.domain.reserve_sensor.reserve_co2_sensor.controller.model.Reserve_Co2Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
@@ -9,6 +11,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,27 +21,27 @@ import java.io.IOException;
 @RequestMapping("/api/boars_co2")
 public class Reserve_Co2ApiController {
 
-    private final UserBusiness userBusiness;
+    private final Reserve_Co2Business reserveCo2Business;
 
     @Autowired
-    public Reserve_Co2ApiController(UserBusiness userBusiness) {
-        this.userBusiness = userBusiness;
+    public Reserve_Co2ApiController(Reserve_Co2Business co2Business) {
+        this.reserveCo2Business = co2Business;
     }
 
-    @GetMapping("/co2data")
-    public UserResponse getCo2SensorData() {
-        return userBusiness.getCo2SensorData();
+    @GetMapping("/co2data/{reserveSowsRoomNum}")
+    public ResponseEntity<Reserve_Co2Response> getCo2SensorData(@PathVariable String reserveSowsRoomNum) {
+        return ResponseEntity.ok(reserveCo2Business.getCo2SensorData(reserveSowsRoomNum));
     }
 
     @GetMapping("/export")
     public ResponseEntity<Resource> exportCo2Data() {
-        String filePath = "co2_data.xlsx";
+        String filePath = "reserve_co2_data.xlsx";
         try {
-            userBusiness.exportCo2DataToExcel(filePath);
+            reserveCo2Business.exportCo2DataToExcel(filePath);
             Resource resource = new FileSystemResource(filePath);
 
             HttpHeaders headers = new HttpHeaders();
-            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=co2_data.xlsx");
+            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=reserve_co2_data.xlsx");
 
             return new ResponseEntity<>(resource, headers, HttpStatus.OK);
         } catch (IOException e) {
