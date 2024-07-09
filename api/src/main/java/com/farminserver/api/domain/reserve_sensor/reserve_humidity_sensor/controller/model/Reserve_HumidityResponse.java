300b057d60fb2c6694c0719fe48698b323920f6c
cheonsebin <lemon2842@naver.com>
2024-07-09 14:30:26 +0900

    Reserve_humidityResponse의 내용 재수정

--- api/src/main/java/com/farminserver/api/domain/reserve_sensor/reserve_humidity_sensor/controller/model/Reserve_HumidityResponse.java
+++ api/src/main/java/com/farminserver/api/domain/reserve_sensor/reserve_humidity_sensor/controller/model/Reserve_HumidityResponse.java
@@ -1,29 +1,37 @@
 package com.farminserver.api.domain.reserve_sensor.reserve_humidity_sensor.controller.model;
 
 public class Reserve_HumidityResponse {
-    private double co2;
+    private String reserveBarnRoomNum;
+    private double ReserveHumidityData;
     private String unit;
     private long timestamp;
 
-    public Reserve_HumidityResponse(double co2, String unit, long timestamp) {
-        this.co2 = co2;
+    public Reserve_HumidityResponse(String reserveBarnRoomNum, double reserveHumidity, String unit, long timestamp) {
+        this.reserveBarnRoomNum = reserveBarnRoomNum;
+        this.ReserveHumidityData = reserveHumidity;
         this.unit = unit;
         this.timestamp = timestamp;
     }
 
     // Getters and Setters
-    public double getCo2() {
-        return co2;
+
+    public String getReserveBarnRoomNum() {
+        return reserveBarnRoomNum;
+    }
+    public void setReserveBarnRoomNum(String reserveBarnRoomNum) {
+        this.reserveBarnRoomNum = reserveBarnRoomNum;
     }
 
-    public void setSensorValue(double co2) {
-        this.co2 = co2;
+    public double getReservesHumidityData() {
+        return ReserveHumidityData;
+    }
+    public void setHumidity(double ReserveHumidity) {
+        this.ReserveHumidityData = ReserveHumidity;
     }
 
     public String getUnit() {
         return unit;
     }
-
     public void setUnit(String unit) {
         this.unit = unit;
     }
@@ -31,7 +39,6 @@ public class Reserve_HumidityResponse {
     public long getTimestamp() {
         return timestamp;
     }
-
     public void setTimestamp(long timestamp) {
         this.timestamp = timestamp;
     }
