7245684c490e711036f4a284fbaf6999c6c2a42d
LeeHonggyo <ghdry2@gmail.com>
2024-07-16 16:48:46 +0900

    fix: fixed AppConfig

--- api/src/main/java/com/farminserver/api/config/AppConfig.java
+++ api/src/main/java/com/farminserver/api/config/AppConfig.java
@@ -1,6 +1,6 @@
 package com.farminserver.api.config;
 
-import com.farminserver.api.util.Boars_ExcelExporter;
+import com.farminserver.api.util.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,8 +9,38 @@ import org.springframework.beans.factory.annotation.Value;
 public class AppConfig {
 
     @Bean
-    public Boars_ExcelExporter excelExporter() {
+    public Boars_ExcelExporter boarsExcelExporter() {
         return new Boars_ExcelExporter();
+    }
+
+    @Bean
+    public Finishing_ExcelExporter finishingExcelExporter() {
+        return new Finishing_ExcelExporter();
+    }
+
+    @Bean
+    public Gestation_ExcelExporter gestationExcelExporter() {
+        return new Gestation_ExcelExporter();
+    }
+
+    @Bean
+    public Growing_ExcelExporter growingExcelExporter() {
+        return new Growing_ExcelExporter();
+    }
+
+    @Bean
+    public Maternity_ExcelExporter maternityExcelExporter() {
+        return new Maternity_ExcelExporter();
+    }
+
+    @Bean
+    public Piglet_ExcelExporter pigletExcelExporter() {
+        return new Piglet_ExcelExporter();
+    }
+
+    @Bean
+    public Reserve_ExcelExporter reserveExcelExporter() {
+        return new Reserve_ExcelExporter();
     }
 
     @Value("${spring.datasource.url}")
