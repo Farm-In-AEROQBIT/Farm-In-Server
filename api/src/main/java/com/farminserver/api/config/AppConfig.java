eb4d6a28b31087000df9c01b6acbe585fa3fe28d
LeeHonggyo <ghdry2@gmail.com>
2024-07-16 17:16:59 +0900

    fix: fixed .env error

--- api/src/main/java/com/farminserver/api/config/AppConfig.java
+++ api/src/main/java/com/farminserver/api/config/AppConfig.java
@@ -2,6 +2,7 @@ package com.farminserver.api.config;
 
 import com.farminserver.api.util.*;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.beans.factory.annotation.Value;
 
@@ -9,52 +10,63 @@ import org.springframework.beans.factory.annotation.Value;
 public class AppConfig {
 
     @Bean
+    @Primary
     public Boars_ExcelExporter boarsExcelExporter() {
         return new Boars_ExcelExporter();
     }
 
     @Bean
+    @Primary
     public Finishing_ExcelExporter finishingExcelExporter() {
         return new Finishing_ExcelExporter();
     }
 
     @Bean
+    @Primary
     public Gestation_ExcelExporter gestationExcelExporter() {
         return new Gestation_ExcelExporter();
     }
 
     @Bean
+    @Primary
     public Growing_ExcelExporter growingExcelExporter() {
         return new Growing_ExcelExporter();
     }
 
     @Bean
+    @Primary
     public Maternity_ExcelExporter maternityExcelExporter() {
         return new Maternity_ExcelExporter();
     }
 
     @Bean
+    @Primary
     public Piglet_ExcelExporter pigletExcelExporter() {
         return new Piglet_ExcelExporter();
     }
 
     @Bean
+    @Primary
     public Reserve_ExcelExporter reserveExcelExporter() {
         return new Reserve_ExcelExporter();
     }
 
     @Value("${spring.datasource.url}")
-    private String databaseUrl;
+    private String SPRING_DATASOURCE_URL;
 
     @Value("${spring.datasource.username}")
-    private String databaseUsername;
+    private String SPRING_DATASOURCE_USERNAME;
 
     @Value("${spring.datasource.password}")
-    private String databasePassword;
+    private String SPRING_DATASOURCE_PASSWORD;
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String SPRING_DATASOURCE_DRIVER_CLASS_NAME;
 
     public void printDatabaseConfig() {
-        System.out.println("URL: " + databaseUrl);
-        System.out.println("Username: " + databaseUsername);
-        System.out.println("Password: " + databasePassword);
+        System.out.println("URL: " + SPRING_DATASOURCE_URL);
+        System.out.println("Username: " + SPRING_DATASOURCE_USERNAME);
+        System.out.println("Password: " + SPRING_DATASOURCE_PASSWORD);
+        System.out.println("Driver ClassName: " + SPRING_DATASOURCE_DRIVER_CLASS_NAME);
     }
 }
